3c720626a73247ea392a7b3fc616926a09b9f06d
LaYaungHtut <layaunghtut5@gmail.com>
2024-12-11 22:02:23 +0630

    hummmmm so tired

--- src/lib/components/carousel/Carousel.svelte
+++ src/lib/components/carousel/Carousel.svelte
@@ -11,25 +11,48 @@
 		swiper = new Swiper('.swiper', {
 			direction: 'horizontal',
 			loop: true,
+			slidesPerView: 3,
+			spaceBetween: 10,
 			pagination: {
-				el: '.swiper-pagination'
+				el: '.swiper-pagination',
+				type: 'bullets'
 			},
+
 			navigation: {
 				nextEl: '.swiper-button-next',
 				prevEl: '.swiper-button-prev'
 			}
 		});
+
+		document.querySelector('.swiper-button-next')?.addEventListener('click', () => {
+			swiper?.slideNext();
+		});
+		document.querySelector('.swiper-button-prev')?.addEventListener('click', () => {
+			swiper?.slidePrev();
+		});
 	});
+
+	let swiperContent = [
+		{ content: 'This is the first slide' },
+		{ content: 'This is the second slide' },
+		{ content: 'This is the third slide' },
+		{ content: 'This is the fourth slide' },
+		{ content: 'This is the fifth slide' }
+	];
 </script>
 
 <main>
 	<div class="swiper">
 		<div class="swiper-wrapper">
-			<div class="swiper-slide"><p>This is slide one from the carousel</p></div>
+			<!-- <div class="swiper-slide">This is slide one from the carousel</div>
 			<div class="swiper-slide">Slide 2</div>
 			<div class="swiper-slide">Slide 3</div>
 			<div class="swiper-slide">SLide 4</div>
-			<div class="swiper-slide">Slide 5</div>
+			<div class="swiper-slide">Slide 5</div> -->
+
+			{#each swiperContent as slide}
+				<div class="swiper-slide">{slide.content}</div>
+			{/each}
 		</div>
 
 		<div class="swiper-pagination"></div>
@@ -46,42 +69,42 @@
 		align-items: center;
 
 		& .swiper {
-			width: 100%;
+			width: 90%;
 			position: relative;
 
 			& .swiper-wrapper {
-				border: 1px solid red;
 				width: fit-content;
 				height: fit-content;
-                justify-content: center;
-                align-items: center;
-                text-align: center;
+				justify-content: center;
+				align-items: center;
+				text-align: center;
 
 				& .swiper-slide {
+					display: flex;
+					justify-content: center;
+					align-items: center;
+					text-align: center;
+					box-sizing: border-box;
 					padding: 20px;
 				}
 			}
 		}
 		& .swiper-pagination {
 			position: absolute;
+            display: block;
 			bottom: 10px;
 			left: 50%;
 			transform: translateX(-50%);
 			text-align: center;
-			width: 100%;
-            background-color: black;
+			width: 10px;
+            height: 10px;
+			background-color: black;
 		}
 		& .swiper-button-next,
-        & .swiper-button-prev {
+		& .swiper-button-prev {
 			color: black;
-			background: rgba(255, 255, 255, 0.5);
 			border-radius: 50%;
-			width: 40px;
-			height: 40px;
-
-           
-            
+			cursor: pointer;
 		}
-		
 	}
 </style>
