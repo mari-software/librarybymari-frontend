aafc2c908c24f9d156006a779af4a18a57cc03dd
LaYaungHtut <layaunghtut5@gmail.com>
2024-12-11 19:03:10 +0630

    npm swiper

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+	import Carousel from '$lib/components/carousel/Carousel.svelte';
 	import Entrance from '$lib/components/entrance/Entrance.svelte';
 	import Footer from '$lib/components/footer/Footer.svelte';
 	import TopNavigation from '$lib/components/navigation/TopNavigation.svelte';
@@ -15,6 +16,8 @@ mainEntranceSource= "Mari_Gold"
 mainEntranceText= "Build your ideas faster"
 mainEntranceButton= "See more"
 />
+
+<Carousel />
 
 <Footer
 navBarLogoSource="https://anibase.net/files/200e4fbbc54f6ffdb2565919c2fa354d"
